3502fc7283078da7247c1679a50c6a6cf7bfe898
duxueyang <duxy@1391.com>
2019-04-02 17:27:03 +0800

    添加项目module

---
+++ AndroidFrame/library_base_t_mvp/src/main/java/com/dxy/library_base_t_mvp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dxy.library_base_t_mvp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
